b0f2388e56b23f6a2d65a80d2ebdf20f8f7037bc
ran <148943216+zhaoran2023@users.noreply.github.com>
2023-11-21 17:28:52 +0800

    Add files via upload

---
+++ global.d.ts
@@ -0,0 +1,4 @@
+declare module '@rsuite/icons/legacy/Spinner'{
+    const spinner :any
+    export default spinner
+}
---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // reactStrictMode:true,
+  // swcMinify:true,
+  // output:"standalone"
+  
+    //  output: 'export',
+    //  assetPrefix: 'https://100.115.22.101/',
+      // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+
+module.exports = nextConfig
